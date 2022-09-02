3cb3b59fa4da288f76f63317f4cead511a9f1c04
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-09-02 10:56:43 +0300

    Meal of the day section completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-
+async function getMealOfTheDay() {
+    return fetch("https://www.themealdb.com/api/json/v1/1/random.php").then(
+      (response) => response.json()
+    );
+  }
+  async function mealData() {
+    console.log();
+    const data = await getMealOfTheDay();
+  
+    return {
+      title: data.meals[0].strMeal,
+      imageUrl: data.meals[0].strMealThumb,
+      instructions: data.meals[0].strInstructions
+        .split("\r\n")
+        .filter((x) => x.length != 0),
+    };
+  }
+  window.addEventListener("DOMContentLoaded", async (event) => {
+    const meal = await mealData();
+    document.getElementById("mealImage").src = meal.imageUrl;
+    document.getElementById("mealTitle").innerHTML = meal.title;
+  
+  
+    let listElement = document.getElementById("mealInstructions");
+    meal.instructions.forEach((instruction) => {
+      const li = document.createElement("li");
+      li.classList.add("list-group-item");
+      li.innerHTML = instruction;
+      listElement.appendChild(li);
+    });
+  });
